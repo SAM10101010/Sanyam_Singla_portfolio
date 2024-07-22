fe10a400c09db2dc6b9e3b8b7861a5a60616b241
sam1002334 <sanyamsingla36@gmail.com>
2024-07-22 21:57:11 +0530

    autoplay song

--- index.js
+++ index.js
@@ -544,26 +544,35 @@ document
     updateCamPos.set(-0.3, 0, 5);
 });
 
-//SOUND TOGGLE
-const backgroundSound = new Audio("./audios/Tauba Tauba - Karan Aujla.mp3");
-let soundOn = false;
-document
-  .querySelector(".sound")
-  .addEventListener("click", function(event) {
-    if (soundOn === true) {
-      this.style.opacity = 1;
-      backgroundSound.pause();
-      this.style.backgroundImage = "url('./icons/icon-mute-96.png')";
-      soundOn = false;
-    } else {
-      this.style.opacity = 0.2;
-      backgroundSound.play();
-      backgroundSound.volume = 0.5;
-      backgroundSound.loop = true;
-      this.style.backgroundImage = "url('./icons/icon-sound-96.png')";
+// SOUND TOGGLE
+const backgroundSound = new Audio("./audios/suzume.mp3");
+let soundOn = true;
+
+document.querySelector(".sound").addEventListener("click", function(event) {
+  if (soundOn) {
+    this.style.opacity = 1;
+    backgroundSound.pause();
+    this.style.backgroundImage = "url('./icons/icon-mute-96.png')";
+    soundOn = false;
+  } else {
+    this.style.opacity = 0.2;
+    backgroundSound.play();
+    backgroundSound.volume = 0.5;
+    backgroundSound.loop = true;
+    this.style.backgroundImage = "url('./icons/icon-sound-96.png')";
     soundOn = true;
-    }
+  }
 });
+
+// Auto-play the background sound and set the initial state of the sound logo when the page loads
+window.addEventListener('load', function() {
+  backgroundSound.play();
+  backgroundSound.volume = 0.5;
+  backgroundSound.loop = true;
+  document.querySelector(".sound").style.backgroundImage = "url('./icons/icon-sound-96.png')";
+  document.querySelector(".sound").style.opacity = 0.2;
+});
+
 
 //WHOOSH SOUND
 document.querySelectorAll("a").forEach((each) => {
